3a53f30a1a03ce06fa2692a5869e2eebe40a8222
gabrielfiskbaek <gar001@edu.zealand.dk>
2025-05-13 12:17:49 +0200

    Correction in ListingNotFoundException

--- src/main/java/com/example/demo/exceptions/listing/ListingNotFoundException.java
+++ src/main/java/com/example/demo/exceptions/listing/ListingNotFoundException.java
@@ -1,13 +1,19 @@
 package com.example.demo.exceptions.listing;
 
 public class ListingNotFoundException extends RuntimeException {
-    private final Long listingId;
+    private final Long listingAdId;
 
-    public ListingNotFoundException(Long listingId) {
-        super("Listing with id " + listingId + " not found");
-        this.listingId = listingId;
+    public ListingNotFoundException(Long listingAdId) {
+        super("Listing with id " + listingAdId + " not found");
+        this.listingAdId = listingAdId;
     }
+
+    public ListingNotFoundException(String message) {
+        super(message);
+        this.listingAdId = null;
+    }
+
     public Long getListingId() {
-        return listingId;
+        return listingAdId;
     }
 }
