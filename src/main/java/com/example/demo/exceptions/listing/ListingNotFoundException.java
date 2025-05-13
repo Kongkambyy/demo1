46602150a464e24822feb9ed3c5f344b853860de
gabrielfiskbaek <gar001@edu.zealand.dk>
2025-05-13 11:54:39 +0200

    Test commit ListingNotFoundException

--- src/main/java/com/example/demo/exceptions/listing/ListingNotFoundException.java
+++ src/main/java/com/example/demo/exceptions/listing/ListingNotFoundException.java
@@ -1,7 +1,13 @@
 package com.example.demo.exceptions.listing;
 
 public class ListingNotFoundException extends RuntimeException {
-    public ListingNotFoundException(String message) {
-        super(message);
+    private final Long listingId;
+
+    public ListingNotFoundException(Long listingId) {
+        super("Listing with id " + listingId + " not found");
+        this.listingId = listingId;
+    }
+    public Long getListingId() {
+        return listingId;
     }
 }
